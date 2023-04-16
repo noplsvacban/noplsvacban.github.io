27679df54b80f8d57677ac0915b40d922bd16cdf
noplsvacban <110901845+noplsvacban@users.noreply.github.com>
2023-04-16 22:50:25 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,57 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
-      const form = document.getElementById('qa-form');
-      const platformSelect = document.getElementById('platform');
-      const otherPlatformDiv = document.getElementById('other-platform-div');
-      const otherPlatformInput = document.getElementById('other-platform');
-      const emailInput = document.getElementById('email');
-      const discordInput = document.getElementById('discord');
+  const form = document.getElementById('qa-form');
+  const platformSelect = document.getElementById('platform');
+  const otherPlatformDiv = document.getElementById('other-platform-div');
+  const otherPlatformInput = document.getElementById('other-platform');
+  const emailInput = document.getElementById('email');
+  const discordInput = document.getElementById('discord');
 
-      platformSelect.addEventListener('change', () => {
-        if (platformSelect.value === 'other') {
-          otherPlatformDiv.style.display = 'block';
-          otherPlatformInput.required = true;
-        } else {
-          otherPlatformDiv.style.display = 'none';
-          otherPlatformInput.required = false;
-        }
-      });
+  platformSelect.addEventListener('change', () => {
+    if (platformSelect.value === 'other') {
+      otherPlatformDiv.style.display = 'block';
+      otherPlatformInput.required = true;
+    } else {
+      otherPlatformDiv.style.display = 'none';
+      otherPlatformInput.required = false;
+    }
+  });
 
-      form.addEventListener('submit', (event) => {
-        event.preventDefault();
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-        const name = document.getElementById('name').value;
-        const age = document.getElementById('age').value;
-        const favGame = document.getElementById('fav-game').value;
-        const platform = document.getElementById('platform').value;
-        const question = document.getElementById('question').value;
-        const email = emailInput.value;
-        const discord = discordInput.value;
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const favGame = document.getElementById('fav-game').value;
+    const platform = document.getElementById('platform').value;
+    const question = document.getElementById('question').value;
+    const email = emailInput.value;
+    const discord = discordInput.value;
 
-        if (!email && !discord) {
-          alert('Please provide at least one contact method (email or Discord).');
-          return;
-        }
+    if (!email && !discord) {
+      alert('Please provide at least one contact method (email or Discord).');
+      return;
+    }
 
-        if (discord) {
-          emailInput.required = false;
-        } else {
-          emailInput.required = true;
-        }
+    if (discord) {
+      emailInput.required = false;
+    } else {
+      emailInput.required = true;
+    }
 
-        if (email) {
-          discordInput.required = false;
-        } else {
-          discordInput.required = true;
-        }
+    if (email) {
+      discordInput.required = false;
+    } else {
+      discordInput.required = true;
+    }
 
-        let payloadContent = `New question from ${name} (${age} years old):\n\nFavorite game: ${favGame}\nPlatform: ${platform}\nQuestion: ${question}`;
+    let payloadContent = `New question from ${name} (${age} years old):\n\nFavorite game: ${favGame}\nPlatform: ${platform}\nQuestion: ${question}`;
 
-        if (email) {
-          payloadContent += `\nEmail: ${email}`;
-        }
+    if (email) {
+      payloadContent += `\nEmail: ${email}`;
+    }
 
-        if (discord) {
-          payloadContent += `\nDiscord username: ${discord}`;
+    if (discord) {
+      payloadContent += `\nDiscord username: ${discord}`;
     }
 
     const payload = {
